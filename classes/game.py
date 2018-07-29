d6dffd2c7c059a4fbe1d32e955f623aa6d07cd66
Alcides Dias <cidesjour13@gmail.com>
2018-07-29 16:43:48 -0300

    Update game.py

--- classes/game.py
+++ classes/game.py
@@ -73,10 +73,28 @@ class Person:
     def choose_item(self):
         i = 1
 
-        print("\n" + bcolors.OKGREEN + bcolors.BOLD + "ITEMS:" + bcolors.ENDC)
+        print(bcolors.OKGREEN + bcolors.BOLD + "ITEMS:" + bcolors.ENDC)
         for item in self.items:
             print("        " + str(i) + ".", item["item"].name + ":", item["item"].description, "(x" + str(item["quantity"]) + ")")
             i += 1
+
+    def choose_target(self, enemies):
+        i = 1
+
+        print()
+        print(bcolors.FAIL + bcolors.BOLD + "    TARGET:" + bcolors.ENDC)
+
+        for enemy in enemies:
+            if enemy.get_hp() != 0:
+                print("        " + str(i) + ".", enemy.name)
+                i += 1
+
+        target_choice = int(input("    Choose target: ")) - 1
+        print()
+
+        return target_choice
+
+
 
     def get_enemy_stats(self):
         hp_bar = ""
@@ -142,3 +160,15 @@ class Person:
         print(bcolors.BOLD + self.name + "    " +
               chp + "/" + str(self.maxhp) + " |" + bcolors.OKGREEN + hp_bar + bcolors.ENDC + bcolors.BOLD
               + "|     " + cmp + "/" + str(self.maxmp) + " |" + bcolors.OKBLUE + mp_bar + bcolors.ENDC + "|")
+
+    def choose_enemy_spell(self):
+        magic_choice = random.randrange(0, len(self.magic))
+        spell = self.magic[magic_choice]
+        magic_dmg = spell.generate_damage()
+        pct = self.hp / self.maxhp * 100
+
+        if self.mp < spell.cost or spell.type == "white" and pct > 50:
+            self.choose_enemy_spell()
+
+        else:
+            return spell, magic_dmg
